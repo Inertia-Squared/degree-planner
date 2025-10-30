4bdd949f1c14b107c8ab201e2f8f8b455bf0614b
Inertia <ojmiddlet@gmail.com>
2025-10-30 19:05:37 +1100

    - Added simple onboarding features

--- subject-planner/src/components/InfoPanel.tsx
+++ subject-planner/src/components/InfoPanel.tsx
@@ -23,28 +23,32 @@ const InfoPanel = ({item, className}: InfoPanelProps) => {
     const [show, setShow] = useState(true);
 
     if (show) {
-        return(<div className={`${className}`}>
+        return(<div className={`flex flex-col ${className}`}>
+
             <div className={`flex flex-row font-bold text-center text-xl items-center mx-2`}>
                 <div className={`hover:cursor-pointer flex-1`} onClick={()=>setShow(!show)}><BsArrowRightShort size={24}/></div>
                 Info Panel
                 <div className={`flex-1`}></div>
             </div>
+            <div className={`text-xs`}>This panel provides info on the selected node. It will be more readable in the future!</div>
             <hr/>
             <hr/>
             <hr/>
-            {entries.map((e)=>{
-                let shouldTerminate = false;
-                hiddenTerms.forEach((t)=>{
-                    if (t == e[0]) shouldTerminate = true;
-                })
-                if (shouldTerminate) return;
+            <div className={`flex flex-col overflow-y-scroll`}>
+                {entries.map((e)=>{
+                    let shouldTerminate = false;
+                    hiddenTerms.forEach((t)=>{
+                        if (t == e[0]) shouldTerminate = true;
+                    })
+                    if (shouldTerminate) return;
 
-                if (e[0].includes('Link')){
-                    return <li key={e[0]} className={`overflow-x-clip`}><strong>{e[0]}</strong>: <a target={'_blank'} className={`underline text-blue-500`} key={e[0]} href={e[1] as string}>WSU Handbook</a></li>
-                } else {
-                    return <li key={e[0]} className={`overflow-x-clip`}><strong>{e[0]}</strong>: <p className={``}>{(e[1] as string).toString()}</p></li>
-                }
-            })}
+                    if (e[0].includes('Link')){
+                        return <li key={e[0]} className={`overflow-x-clip`}><strong>{e[0]}</strong>: <a target={'_blank'} className={`underline text-blue-500`} key={e[0]} href={e[1] as string}>WSU Handbook</a></li>
+                    } else {
+                        return <li key={e[0]} className={`overflow-x-clip`}><strong>{e[0]}</strong>: <p className={``}>{(e[1] as string).toString()}</p></li>
+                    }
+                })}
+            </div>
         </div>)
     } else {
         return (<div className={`hover:cursor-pointer z-20 absolute right-0 top-4/5 bottom-1/5 border rounded-l-md w-8 h-8`} onClick={()=>setShow(!show)}><BsArrowLeftShort size={32}/></div>);
