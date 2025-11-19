7665ada06028096a3166d924c538ed63c0e7c954
Oliver <ojmiddlet@gmail.com>
2025-11-19 23:48:19 +1100

    - Fix help tab getting squished

--- subject-planner/src/app/page.tsx
+++ subject-planner/src/app/page.tsx
@@ -605,7 +605,7 @@ export default function Home() {
             <div onClick={()=> {
                 setShowHelp(!showHelp);
                 setFirstShowHelp(false);
-            }} className={`absolute right-0 top-24 z-31 flex flex-row ${!showHelp ? `max-h-8 items-center border border-r-0 rounded-l-md bg-white ${(firstShowHelp) ? 'animate-bounceright w-12 translate-x-0 !bg-green-300' : 'w-8'}` : ''}`}><BsQuestion className={`${showHelp ? `max-h-8 items-center border border-r-0 rounded-l-md bg-white` : ''}`} size={32}/>{showHelp && <div className={`border rounded-bl-lg px-1.5 max-w-[400px] min-w-[250px] overflow-y-scroll bg-white`}>
+            }} className={`absolute right-0 top-24 z-31 flex flex-row ${!showHelp ? `max-h-8 items-center border border-r-0 rounded-l-md bg-white ${(firstShowHelp) ? 'animate-bounceright w-12 translate-x-0 !bg-green-300' : 'w-8'}` : ''}`}><BsQuestion className={`${showHelp ? `max-h-8 items-center border border-r-0 rounded-l-md bg-white` : ''}`} size={32}/>{showHelp && <div className={`border rounded-bl-lg px-1.5 max-w-[400px] w-full min-w-[250px] overflow-y-scroll bg-white`}>
                 Welcome to <strong>MyDegree.help!</strong> To get started, you can type part or all of a program name into the search bar, the dropdown will fill automatically with any matching courses.
                 <br/><br/> Selecting a Major or Minor is optional (if you don't want one, just don't select it), once you are happy with your choices, click 'Start Exploring' to plan your degree!
                 <br/><br/><strong>IMPORTANT:</strong>
