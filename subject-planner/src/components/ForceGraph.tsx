6f6d5c2e7e446fdd53178f61cce1f191caf33187
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2025-12-08 02:41:54 +1100

    Add ref to control camera when nodes shown

--- subject-planner/src/components/ForceGraph.tsx
+++ subject-planner/src/components/ForceGraph.tsx
@@ -1,6 +1,7 @@
 // components/ForceGraph.js
-import {GraphCanvas, GraphEdge, LayoutTypes} from 'reagraph';
-import {ExtendedNode} from "@/app/page";
+import {GraphCanvas, GraphCanvasRef, GraphEdge, LayoutTypes} from 'reagraph';
+import { useEffect, useRef } from 'react';
+import { ExtendedNode } from '@/utils/types';
 
 interface ForceGraphProps {
     nodes: ExtendedNode<any>[],
@@ -16,10 +17,20 @@ interface ForceGraphProps {
 
 const ForceGraph = ({nodes, edges, className, doubleClickNodeAction, clusterBy, clickAction, clickCanvas, layoutMode, collapsedNodeIds}: ForceGraphProps) => {
     const classN = className ?? `w-[300px] h-full relative`;
+    const graphRef = useRef<GraphCanvasRef | null >(null);
+    useEffect(() => {
+        if (nodes.length > 0) {
+            const timer = setTimeout(() => {
+                graphRef.current?.fitNodesInView();
+            }, 100);
+            return () => clearTimeout(timer);
+        }
+    }, [nodes]);
     return (
         <div className={classN}>
-            {clusterBy &&  <GraphCanvas cameraMode={'pan'} collapsedNodeIds={collapsedNodeIds ?? []} labelType={'nodes'} layoutType={layoutMode} clusterAttribute={(layoutMode === 'forceDirected2d') ? clusterBy : undefined} draggable={true} onCanvasClick={clickCanvas} onNodeClick={(node)=>clickAction(node.id)} onEdgeClick={(edge)=>clickAction(edge.id, false)} onNodeDoubleClick={(node) => doubleClickNodeAction(node.id)} nodes={nodes} edges={edges}/>}
-            {!clusterBy &&  <GraphCanvas cameraMode={'pan'} collapsedNodeIds={collapsedNodeIds ?? []} labelType={'nodes'} layoutType={layoutMode} draggable={true} onCanvasClick={clickCanvas} onNodeClick={(node)=>clickAction(node.id)} onEdgeClick={(edge)=>clickAction(edge.id, false)} onNodeDoubleClick={(node) => doubleClickNodeAction(node.id)} nodes={nodes} edges={edges}/>}
+            {clusterBy ? <GraphCanvas ref={graphRef} cameraMode={'pan'} collapsedNodeIds={collapsedNodeIds ?? []} labelType={'nodes'} layoutType={layoutMode} clusterAttribute={(layoutMode === 'forceDirected2d') ? clusterBy : undefined} draggable={true} onCanvasClick={clickCanvas} onNodeClick={(node)=>clickAction(node.id)} onEdgeClick={(edge)=>clickAction(edge.id, false)} onNodeDoubleClick={(node) => doubleClickNodeAction(node.id)} nodes={nodes} edges={edges}/>
+            : <GraphCanvas ref={graphRef} cameraMode={'pan'} collapsedNodeIds={collapsedNodeIds ?? []} labelType={'nodes'} layoutType={layoutMode} draggable={true} onCanvasClick={clickCanvas} onNodeClick={(node)=>clickAction(node.id)} onEdgeClick={(edge)=>clickAction(edge.id, false)} onNodeDoubleClick={(node) => doubleClickNodeAction(node.id)} nodes={nodes} edges={edges}/>
+                        }
         </div>
     );
 };
