067c4eb2bee798411fed5a037433b604c0540011
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2026-03-06 23:26:46 +1100

    Add icons on header

--- subject-planner/src/components/ui/layout/Headers/TimelineHeader.tsx
+++ subject-planner/src/components/ui/layout/Headers/TimelineHeader.tsx
@@ -1,13 +1,16 @@
-import {HeaderItem, HeaderItemProps, shouldShowItem} from "@/components/ui/layout/Containers/HeaderBar";
+import { HeaderItem, HeaderItemProps, shouldShowItem } from "@/components/ui/layout/Containers/HeaderBar";
+import { RiTimeLine } from "react-icons/ri";
 
-const TimelineHeader = ({onHeaderClicked, selectedHeaderItem,}: HeaderItemProps) => {
+const TimelineHeader = ({ onHeaderClicked, selectedHeaderItem }: HeaderItemProps) => {
     const itemIdentifier = HeaderItem.TIMELINE;
     return (
-        <div
-            onClick={() => onHeaderClicked(itemIdentifier)}
-            className="header-button"
-        >
-            <span className={`${shouldShowItem(selectedHeaderItem, itemIdentifier) && "header-button-selected"}`}>Timeline</span>
+        <div onClick={() => onHeaderClicked(itemIdentifier)} className="header-button">
+            <div
+                className={`${shouldShowItem(selectedHeaderItem, itemIdentifier) && "header-button-selected"} flex space-x-2 justify-start items-center`}
+            >
+                <RiTimeLine />
+                <span>Timeline</span>
+            </div>
         </div>
     );
 };
